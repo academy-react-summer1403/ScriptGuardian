edd88330a61c0eba1fd6d300daa3f73141cde5a2
AMIR <kolafmob@gmail.com>
2024-10-19 17:11:34 +0330

    darkmode

--- src/components/common/TopHeader/MainMenu.jsx
+++ src/components/common/TopHeader/MainMenu.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const MainMenu = () => {
   return (
     <ul className="flex xl:text-[16px] xl:gap-[48px] lg:gap-[42px]  md:gap-[30px]  text-[#263238] dark:text-white">
       <li className="relative block  cursor-pointer before:content-[''] before:absolute before:right-0 before:left-full before:bottom-[-3px] before:h-1 before:bg-[#2196F3] dark:before:bg-[#0D47A1] before:transition-all before:duration-300 before:ease-in-out hover:before:right-0 hover:before:left-0 ">
-        دوره ها
+        <NavLink to={"/Courses"}>دوره ها</NavLink>{" "}
       </li>
       <li className="relative block  cursor-pointer before:content-[''] before:absolute before:right-0 before:left-full before:bottom-[-3px] before:h-1 before:bg-[#2196F3] dark:before:bg-[#0D47A1] before:transition-all before:duration-300 before:ease-in-out hover:before:right-0 hover:before:left-0 ">
         اساتید
@@ -13,7 +14,7 @@ const MainMenu = () => {
         ارتباط با ما
       </li>
       <li className="relative block  cursor-pointer before:content-[''] before:absolute before:right-0 before:left-full before:bottom-[-3px] before:h-1 before:bg-[#2196F3] dark:before:bg-[#0D47A1] before:transition-all before:duration-300 before:ease-in-out hover:before:right-0 hover:before:left-0 ">
-        اخبار مقالات
+        <NavLink to={"/News"}>اخبار مقالات</NavLink>
       </li>
     </ul>
   );
