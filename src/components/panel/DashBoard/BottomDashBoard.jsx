7b7664e12a0a4f191338101d36388adb5d9b4a48
AMIR <kolafmob@gmail.com>
2024-11-07 20:57:03 +0330

    HandelDashBoardDetails

---
+++ src/components/panel/DashBoard/BottomDashBoard.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import { FaMinus } from "react-icons/fa";
+import CourseImg from "../../../images/StudentPanel/DashBoard/Untitled.jpg";
+import { useCourses } from "../../../core/services/api/CoursesPage/GetAllCourses";
+import { useLandingNews } from "../../../core/services/api/Landing/LandingNews";
+import Image from "../../../images/NewsDetails/default_image.png";
+import { useNavigate } from "react-router-dom";
+const BottomDashBoard = () => {
+  const navigate = useNavigate();
+  //API COURSES
+
+  const { data } = useCourses();
+  const lastTwoItems = data?.slice(-2);
+  console.log(lastTwoItems, "lastTwoItems");
+
+  //API NEWS
+  const { data: News, isPending } = useLandingNews();
+  const lastNews = News?.slice(-2);
+
+  return (
+    <div className="flex sm:flex-row flex-col w-[95%] mt-10 gap-x-10 ">
+      {/* Courses */}
+
+      <div className="flex flex-col sm:w-[47.5%] ">
+        <div className="flex items-center text-xl gap-x-1 ">
+          {" "}
+          <FaMinus className="text-xl text-purple-600 dark:text-purple-800" />
+          <h3 className="text-[#263238] dark:text-gray-200 font-bold">
+            {" "}
+            اخرین دوره ثبت شده
+          </h3>
+        </div>
+
+        <div className="flex flex-col w-full mt-5 gap-y-5 ">
+          {/* Items */}
+          {lastTwoItems?.map((item, index) => {
+            return (
+              <>
+                <div
+                  className="flex w-full bg-white dark:bg-gray-900 rounded-md h-[75px] shadow-ّFirst-shadow  items-center border-gray-200 dark:border-gray-950 cursor-pointer"
+                  key={index}
+                  onClick={() => {
+                    navigate(
+                      `/courses/${item?.courseId ? item?.courseId : "no ID"}`
+                    );
+                  }}
+                >
+                  <div className="h-[85%] w-[25%] mr-[3%] ">
+                    <img
+                      src={
+                        item?.tumbImageAddress ? item?.tumbImageAddress : Image
+                      }
+                      alt=""
+                      className="w-full h-full rounded-md"
+                    />
+                  </div>
+                  <div className="flex w-[100%] h-[85%] flex-col justify-between ">
+                    <h3 className="mr-3 text-[#263238] dark:text-gray-200">
+                      {item?.title ? item?.title : ""}
+                    </h3>
+                    <div className="flex justify-between text-[#455A64] dark:text-gray-400 items-center mr-3">
+                      <p className="text-sm">
+                        {" "}
+                        {item?.teacherName ? item?.teacherName : ""}
+                      </p>
+
+                      <p className="text-sm ml-3">
+                        <span className="text-purple-700 dark:text-purple-500">
+                          {item?.cost ? item?.cost : ""}
+                        </span>{" "}
+                        تومان
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* News */}
+
+      <div className="flex flex-col sm:w-[47.5%] sm:mt-0 mt-5 mb-5">
+        <div className="flex items-center text-xl gap-x-1 ">
+          {" "}
+          <FaMinus className="text-xl text-purple-600 dark:text-purple-800" />
+          <h3 className="text-[#263238] dark:text-gray-200 font-bold">
+            {" "}
+            آخرین اخبار ثبت شده{" "}
+          </h3>
+        </div>
+
+        <div className="flex flex-col w-full mt-5 gap-y-5 ">
+          {lastNews?.map((item, index) => {
+            return (
+              <>
+                <div
+                  className="flex w-full bg-white dark:bg-gray-900 rounded-md h-[75px] shadow-ّFirst-shadow  items-center border-gray-200 dark:border-gray-950 cursor-pointer"
+                  key={index}
+                  onClick={() => {
+                    navigate(`/News/${item?.id ? item?.id : "no ID"}`);
+                  }}
+                >
+                  <div className="h-[85%] w-[25%] mr-[3%] ">
+                    <img
+                      src={
+                        item?.currentImageAddressTumb
+                          ? item?.currentImageAddressTumb
+                          : Image
+                      }
+                      alt=""
+                      className="w-full h-full rounded-md"
+                    />
+                  </div>
+                  <div className="flex w-[100%] h-[85%] flex-col justify-between ">
+                    <h3 className="mr-3 text-[#263238] dark:text-gray-200">
+                      {item?.title ? item?.title : ""}
+                    </h3>
+                    <div className="flex justify-between text-[#455A64] dark:text-gray-400 items-center mr-3">
+                      <p className="text-sm">
+                        {" "}
+                        {item?.addUserFullName ? item?.addUserFullName : ""}
+                      </p>
+
+                      {/* <p className="text-sm ml-3">
+                        <span className="text-purple-700 dark:text-purple-500">
+                          500,000
+                        </span>{" "}
+                        تومان
+                      </p> */}
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { BottomDashBoard };
--- src/screens/StudentPanel/DashBoard/DashBoard.jsx
+++ src/screens/StudentPanel/DashBoard/DashBoard.jsx
@@ -22,6 +22,7 @@ import { Link, NavLink } from "react-router-dom";
 import userProfile from "../../.././images/StudentPanel/NavStudent/images.png";
 import CourseImg from "../../../images/StudentPanel/DashBoard/Untitled.jpg";
 import { UserInFormation } from "../../../components/panel/DashBoard/UserInFormation";
+import { BottomDashBoard } from "../../../components/panel/DashBoard/BottomDashBoard";
 
 const DashBoard = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -160,143 +161,9 @@ const DashBoard = () => {
 
         {/* UserInform */}
         <UserInFormation />
-
+          
         {/* UserCourses */}
-        <div className="flex sm:flex-row flex-col w-[95%] mt-10 gap-x-10 ">
-          {/* Right */}
-          <div className="flex flex-col sm:w-[47.5%] ">
-            <div className="flex items-center text-xl gap-x-1 ">
-              {" "}
-              <FaMinus className="text-xl text-purple-600 dark:text-purple-800" />
-              <h3 className="text-[#263238] dark:text-gray-200 font-bold">
-                {" "}
-                اخرین دوره ثبت شده
-              </h3>
-            </div>
-
-            <div className="flex flex-col w-full mt-5 gap-y-5 ">
-              {/* Items */}
-              <div className="flex w-full bg-white dark:bg-gray-900 rounded-md h-[75px] shadow-ّFirst-shadow  items-center border-gray-200 dark:border-gray-950">
-                <div className="h-[85%] w-[25%] mr-[3%] ">
-                  <img
-                    src={CourseImg}
-                    alt=""
-                    className="w-full h-full rounded-md"
-                  />
-                </div>
-                <div className="flex w-[100%] h-[85%] flex-col justify-between ">
-                  <h3 className="mr-3 text-[#263238] dark:text-gray-200">
-                    آموزش رایگان اچ تی ام ال
-                  </h3>
-                  <div className="flex justify-between text-[#455A64] dark:text-gray-400 items-center mr-3">
-                    <p className="text-sm">دکتر بحر</p>
-
-                    <p className="text-sm ml-3">
-                      <span className="text-purple-700 dark:text-purple-500">
-                        500,000
-                      </span>{" "}
-                      تومان
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              {/* Items */}
-              <div className="flex w-full bg-white dark:bg-gray-900 rounded-md h-[75px] shadow-ّFirst-shadow  items-center border-gray-200 dark:border-gray-950">
-                <div className="h-[85%] w-[25%] mr-[3%] ">
-                  <img
-                    src={CourseImg}
-                    alt=""
-                    className="w-full h-full rounded-md"
-                  />
-                </div>
-                <div className="flex w-[100%] h-[85%] flex-col justify-between ">
-                  <h3 className="mr-3 text-[#263238] dark:text-gray-200">
-                    آموزش رایگان اچ تی ام ال
-                  </h3>
-                  <div className="flex justify-between text-[#455A64] dark:text-gray-400 items-center mr-3">
-                    <p className="text-sm">دکتر بحر</p>
-
-                    <p className="text-sm ml-3">
-                      <span className="text-purple-700 dark:text-purple-500">
-                        500,000
-                      </span>{" "}
-                      تومان
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* Left */}
-
-          <div className="flex flex-col sm:w-[47.5%] sm:mt-0 mt-5 mb-5">
-            <div className="flex items-center text-xl gap-x-1 ">
-              {" "}
-              <FaMinus className="text-xl text-purple-600 dark:text-purple-800" />
-              <h3 className="text-[#263238] dark:text-gray-200 font-bold">
-                {" "}
-                دوره های پیشنهادی
-              </h3>
-            </div>
-
-            <div className="flex flex-col w-full mt-5 gap-y-5 ">
-              {/* Items */}
-              <div className="flex w-full bg-white dark:bg-gray-900 rounded-md h-[75px] shadow-ّFirst-shadow  items-center border-gray-200 dark:border-gray-950">
-                <div className="h-[85%] w-[25%] mr-[3%] ">
-                  <img
-                    src={CourseImg}
-                    alt=""
-                    className="w-full h-full rounded-md"
-                  />
-                </div>
-                <div className="flex w-[100%] h-[85%] flex-col justify-between ">
-                  <h3 className="mr-3 text-[#263238] dark:text-gray-200">
-                    آموزش رایگان اچ تی ام ال
-                  </h3>
-                  <div className="flex justify-between text-[#455A64] dark:text-gray-400 items-center mr-3">
-                    <p className="text-sm">دکتر بحر</p>
-
-                    <p className="text-sm ml-3">
-                      <span className="text-purple-700 dark:text-purple-500">
-                        500,000
-                      </span>{" "}
-                      تومان
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              {/* Items */}
-              {/* Items */}
-              <div className="flex w-full bg-white dark:bg-gray-900 rounded-md h-[75px] shadow-ّFirst-shadow  items-center border-gray-200 dark:border-gray-950">
-                <div className="h-[85%] w-[25%] mr-[3%] ">
-                  <img
-                    src={CourseImg}
-                    alt=""
-                    className="w-full h-full rounded-md"
-                  />
-                </div>
-                <div className="flex w-[100%] h-[85%] flex-col justify-between ">
-                  <h3 className="mr-3 text-[#263238] dark:text-gray-200">
-                    آموزش رایگان اچ تی ام ال
-                  </h3>
-                  <div className="flex justify-between text-[#455A64] dark:text-gray-400 items-center mr-3">
-                    <p className="text-sm">دکتر بحر</p>
-
-                    <p className="text-sm ml-3">
-                      <span className="text-purple-700 dark:text-purple-500">
-                        500,000
-                      </span>{" "}
-                      تومان
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+          <BottomDashBoard/>
       </div>
     </>
   );
