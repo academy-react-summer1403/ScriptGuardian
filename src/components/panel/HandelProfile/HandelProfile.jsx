89ec3b8de880517c804c2f5f944b47e18bfe1f9c
AMIR <kolafmob@gmail.com>
2024-12-05 20:37:30 +0330

    +++++

--- src/components/panel/HandelProfile/HandelProfile.jsx
+++ src/components/panel/HandelProfile/HandelProfile.jsx
@@ -9,8 +9,8 @@ import { Gallery } from "./child/Gallery";
 const HandelProfile = ({ data }) => {
   const queryClient = useQueryClient();
 
-  // State برای ذخیره تصویر انتخاب شده
   const [selectedImage, setSelectedImage] = useState(null);
+  const [showSend, setShowSend] = useState(false);
 
   //API
   const { mutate: AddProfile } = useAddProfileImage();
@@ -28,6 +28,7 @@ const HandelProfile = ({ data }) => {
             toast.success("    عکس شما با موفقیت به گالری اتان ارسال شد");
             queryClient.invalidateQueries("GetStudentProfile");
             setSelectedImage(null);
+            setShowSend(false);
           } else {
             toast.error(" ناموفق بود");
           }
@@ -65,7 +66,7 @@ const HandelProfile = ({ data }) => {
 
   return (
     <>
-      <div className="mt-10 flex flex-col sm:items-start item-center justify-center ">
+      {/* <div className="mt-10 flex flex-col sm:items-start item-center justify-center  ">
         <div className="w-[200px] h-[200px]">
           {" "}
           <img
@@ -83,49 +84,109 @@ const HandelProfile = ({ data }) => {
             className="w-full h-full rounded-full"
           />
         </div>
-        <form
-          onSubmit={formik.handleSubmit}
-          className="flex flex-col  items-center  w-full"
-        >
-          <label
-            className="flex justify-center items-center mt-3 text-[#455A64] dark:text-gray-400 cursor-pointer font-medium hover:text-[#1E2A35] hover:scale-105 transform transition duration-300 ease-in-out px-4 py-2 rounded-md shadow-md hover:shadow-lg dark:bg-gray-800 bg-gray-100"
-            htmlFor="formFile"
-            name="formFile"
+        <div className="flex items-center">
+          <form
+            onSubmit={formik.handleSubmit}
+            className="flex   items-center  w-full"
           >
-            اضافه کردن تصویر
-          </label>
-          <input
-            type="file"
-            id="formFile"
-            name="formFile"
-            className="hidden"
-            onChange={(event) => {
-              const file = event.currentTarget.files[0];
+            <label
+              className="flex justify-center items-center mt-3 text-[#455A64] dark:text-gray-400 cursor-pointer font-medium hover:text-[#1E2A35] hover:scale-105 transform transition duration-300 ease-in-out px-4 py-2 rounded-md shadow-md hover:shadow-lg dark:bg-gray-800 bg-gray-100"
+              htmlFor="formFile"
+              name="formFile"
+            >
+              اضافه کردن تصویر
+            </label>
+            <input
+              type="file"
+              id="formFile"
+              name="formFile"
+              className="hidden"
+              onChange={(event) => {
+                const file = event.currentTarget.files[0];
 
-              formik.setFieldValue("formFile", event.currentTarget.files[0]);
-              setSelectedImage(URL.createObjectURL(file));
-            }}
+                formik.setFieldValue("formFile", event.currentTarget.files[0]);
+                setSelectedImage(URL.createObjectURL(file));
+              }}
+            />
+
+            <button
+              type="submit"
+              className="block px-6 py-3 mt-4 bg-[#2196F3] text-white font-semibold rounded-lg shadow-lg dark:bg-blue-800 transition duration-300 ease-in-out"
+            >
+              ارسال
+            </button>
+          </form>
+
+          <h3 className="mx-auto mt-3 text-[#455A64] dark:text-gray-400 text-lg font-semibold flex justify-center items-center space-x-2 ">
+            <button
+              onClick={HandelGalleryModal}
+              className="px-6 py-3 mt-1 mb-5 bg-[#4CAF50] text-white font-medium rounded-lg shadow-md hover:bg-[#45A049] dark:bg-[#388E3C] dark:hover:bg-[#2C6F3F] transition duration-300 ease-in-out transform hover:scale-105"
+            >
+              گالری
+            </button>
+          </h3>
+        </div>
+      </div> */}
+
+      <div className="mt-10 flex flex-col items-center sm:items-start justify-center space-y-6  mx-auto">
+        <div className="w-[200px] h-[200px] mx-auto">
+          <img
+            src={
+              selectedImage
+                ? selectedImage
+                : data?.currentPictureAddress &&
+                  data?.currentPictureAddress !== "Not-set"
+                ? data?.currentPictureAddress
+                : Image
+            }
+            alt="Profile"
+            className="w-full h-full rounded-full border-4 border-gray-200 shadow-md object-cover"
           />
+        </div>
 
-          <button
-            type="submit"
-            className="block px-6 py-3 mt-4 bg-[#2196F3] text-white font-semibold rounded-lg shadow-lg dark:bg-blue-800 transition duration-300 ease-in-out"
-          >
-            ارسال
-          </button>
-        </form>
-
-        <h3 className="mx-auto mt-3 text-[#455A64] dark:text-gray-400 text-lg font-semibold flex justify-center items-center space-x-2">
+        <div className="w-full flex flex-row items-center justify-between gap-4">
           <button
             onClick={HandelGalleryModal}
-            className="px-6 py-3 mt-1 mb-5 bg-[#4CAF50] text-white font-medium rounded-lg shadow-md hover:bg-[#45A049] dark:bg-[#388E3C] dark:hover:bg-[#2C6F3F] transition duration-300 ease-in-out transform hover:scale-105"
+            className="px-6 py-3 bg-[#4CAF50] text-white font-medium rounded-lg shadow-md hover:bg-[#45A049] dark:bg-[#388E3C] dark:hover:bg-[#2C6F3F] transition duration-300 ease-in-out transform hover:scale-105"
           >
             گالری
           </button>
-        </h3>
+
+          <form
+            onSubmit={formik.handleSubmit}
+            className="flex items-center justify-center"
+          >
+            <label
+              htmlFor="formFile"
+              className="text-[#455A64] dark:text-gray-400 cursor-pointer font-medium px-5 py-3 rounded-lg bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 hover:shadow-md dark:hover:bg-gray-700 transition duration-300 ease-in-out transform hover:scale-105"
+            >
+              اضافه کردن تصویر
+            </label>
+            <input
+              type="file"
+              id="formFile"
+              name="formFile"
+              className="hidden"
+              onChange={(event) => {
+                const file = event.currentTarget.files[0];
+                formik.setFieldValue("formFile", event.currentTarget.files[0]);
+                setSelectedImage(URL.createObjectURL(file));
+                setShowSend(true);
+              }}
+            />
+          </form>
+
+          {showSend && (
+            <button
+              type="submit"
+              className="px-6 py-3 rounded-lg bg-[#2196F3] text-white font-semibold shadow-md hover:bg-[#1E88E5] hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105"
+            >
+              ارسال
+            </button>
+          )}
+        </div>
       </div>
 
-      {/* مدال گالری */}
       {isGalleryModalOpen && (
         <Gallery
           gallery={data}
